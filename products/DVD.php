e8027e495b7404d4639e07ff3e4c5f4c0b9c7ebe
Imelda Malcellari <144271408+imelda-m@users.noreply.github.com>
2024-08-09 21:49:35 +0200

    Update DVD.php

--- products/DVD.php
+++ products/DVD.php
@@ -1,36 +1,24 @@
 <?php
-    // Including abstract Product class.
-    require_once 'Product.php';
+require_once 'Product.php';
 
-    // DVD Product class based on abstract Product class.
-    class DVD extends Product {
+class DVD extends Product {
+    private $size;
 
-        private $size;
-
-        public function __construct($sku, $name, $price, $size) {
-            parent::__construct($sku, $name, $price);
-            $this->size = $size;
-        }
-    
-        // Function to get size of the DVD.
-        protected function &getSize($args) {
-            return $args['size'];
-        }
-
-    
-        // Function for DVD to be displayed on Product List.
-        public function displayP($row)
-        {
-            $price = number_format((float)$row['price'], 2, '.', '');
-            echo <<< PRODUCT
-                <div class='product'>
-                    <input class='delete-checkbox' type='checkbox' name='deleteCheckbox[]' value={$row['sku']}>
-                    <span>{$row['sku']}</span>
-                    <span>{$row['name']}</span>
-                    <span>{$price} $</span>
-                    <span>{$row['size']} MB</span>
-                </div>
-PRODUCT;
-        }
+    public function __construct($sku, $name, $price, $size) {
+        parent::__construct($sku, $name, $price, 'DVD');
+        $this->size = $size;
     }
+
+    public function getDetails() {
+        return [
+            'size' => $this->size . ' MB'
+        ];
+    }
+   
+
+    public static function createFromForm($formData) {
+        return new self($formData['skuInput'], $formData['nameInput'], $formData['price'], $formData['size']);
+    }
+}
+
 ?>
