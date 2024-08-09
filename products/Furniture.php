94fcde25878887caed0961f346ec82f65750c2ee
Imelda Malcellari <144271408+imelda-m@users.noreply.github.com>
2024-08-09 21:50:21 +0200

    Update Furniture.php

--- products/Furniture.php
+++ products/Furniture.php
@@ -1,41 +1,49 @@
 <?php
+require_once 'Product.php';
 
-   require_once 'Product.php';
-
-   class Furniture extends Product{
+class Furniture extends Product {
     private $height;
     private $width;
     private $length;
 
-    public function __construct($sku, $name, $price, $height, $width, $length) {
-        parent::__construct($sku, $name, $price);
+    public function __construct($sku, $name, $price, $height, $width , $length) {
+        parent::__construct($sku, $name, $price, 'Furniture');
         $this->height = $height;
         $this->width = $width;
         $this->length = $length;
+
+        
     }
 
-      public function &getHeight($args){
-        return $args['height'];
-      }
-      public function &getWidtht($args){
-        return $args['width'];
-      }
-      public function &getLength($args){
-        return $args['length'];
-      }
+    public function getDetails() {
+       
+       
+        
+        return [
+           'Dimensions' => $this->height . 'x' . $this->width . 'x' . $this->length
+        //    'height' => $this->height,
+        //    'width' => $this->width,
+        //    'length' => $this->length,
+        ];
+    
+    }
+    
 
-      public function displayP($row){
-           $price = number_format((float)$row['price'],2,'-', '');
-           echo <<< PRODUCT
-           <div class='product'>
-               <input class='delete-checkbox' type='checkbox' name='deleteCheckbox[]' value={$row['sku']}>
-               <span>{$row['sku']}</span>
-               <span>{$row['name']}</span>
-               <span>{$price} $</span>
-               <span>{$row['height']}x{$row['width']}x{$row['length']} CM</span> 
-           </div>
-PRODUCT;
-      }
-   }
+    public function renderDetails() {
+        return '<p>Dimensions: ' . $this->height . 'x' . $this->width . 'x' . $this->length . '</p>';
+    }
+
+    public static function createFromForm($formData) {
+        
+        return new self(
+            $formData['skuInput'], 
+            $formData['nameInput'], 
+            $formData['price'], 
+            $formData['height'], 
+            $formData['width'], 
+            $formData['length']
+        );
+    }
+}
 
 ?>
